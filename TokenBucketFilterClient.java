760907f2a308c2d0643ae3c4de23660de68247df
Amit Prabhudesai <amit.prabhudesai@iminmobi.com>
2020-11-05 12:55:56 +0530

    Initial import - token bucket filter

---
+++ TokenBucketFilterClient.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TokenBucketFilterClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        TokenBucketFilter filter = new TokenBucketFilter(1);
+        DaemonTokenBucketFilter dtbf = new DaemonTokenBucketFilter(1);
+        Set<Thread> allThreads = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //filter.getToken();
+                        dtbf.getToken();
+                    } catch (InterruptedException ie) {
+                        // ignore, for now
+                    }
+                }
+            }); 
+            thread.setName("thread_" + i+1);
+            allThreads.add(thread);
+        }
+
+        for (Thread t : allThreads) t.start();
+        for (Thread t : allThreads) t.join();
+    }
+}
